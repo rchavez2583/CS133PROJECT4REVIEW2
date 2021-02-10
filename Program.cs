6264b8959346ef2d6d8faa9ccc71d3a98aaa9b8c
rchavez2583 <77551911+rchavez2583@users.noreply.github.com>
2021-02-10 09:38:04 -0700

    Add files via upload
    Add Corrections

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CS133PROJECT4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cake myCake = new Cake();
+
+            myCake.Type = "Wedding Cake";
+            myCake.AddFlavor<string>(" Vanilla");
+
+            Cake myCake2 = new Cake();
+            myCake2.Type = "Party Cake";
+            myCake2.AddFlavor<string>(" Chocolate ");
+
+        }
+    }
+}
+
+public class Cake
+{
+    public string Flavor { get; set; }
+    public string Type { get; set; }
+
+    public void AddFlavor<C>(C flavor1)
+    
+    {
+        this.Flavor = flavor1 as string;
+
+        
+    }
+
+
+
+
+}
